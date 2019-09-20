e024dbf4b9cb665fe4af8fcec9b9b2dc456db865
NhanJS <nhan.htmljs@gmail.com>
2019-09-20 16:27:46 +0700

    Good dashboard

---
+++ LogContract/Models/SaleLeaderBoardDataPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Retyped.canvasjs.CanvasJS;
+
+namespace LogContract.Models
+{
+    public class SaleLeaderBoardDataPoint : ChartDataPoint
+    {
+        public double gdp { get; set; }
+        public string url { get; set; }
+    }
+}
--- LogOne/NghiepVu/Dashboard/ThongKe.View.cs
+++ LogOne/NghiepVu/Dashboard/ThongKe.View.cs
@@ -1,4 +1,6 @@
-﻿using Components;
+﻿using Bridge.Html5;
+using Components;
+using LogContract.Models;
 using MVVM;
 using static Retyped.canvasjs.CanvasJS;
 
@@ -67,17 +69,23 @@ namespace LogOne.NghiepVu.Dashboard
 
         private void ChartPanels()
         {
-            Html.Instance.Grid().GridRow();
+            Html.Instance.Div.Id(nameof(ThongKe)).Grid().GridRow();
             Customers();
             SaleAnalysis();
+            SaleLeaderBoard();
+            MarketShare();
+            MonthlySaleData();
+            MonthlyExpense();
         }
 
         private static void Customers()
         {
-            Html.Instance.GridCell(6).Div.Id("Fortune500").End.End.Render();
+            string id = nameof(Customers);
+            Html.Instance.GridCell(4).Panel().Div.Id(id).EndOf(".cell");
 
-            var chart = new Chart("Fortune500", new ChartOptions
+            var chart = new Chart(id, new ChartOptions
             {
+                height = 300,
                 animationEnabled = true,
                 title = new ChartTitleOptions { text = "Top customers" },
                 axisX = new ChartAxisXOptions { interval = 1 },
@@ -96,16 +104,6 @@ namespace LogOne.NghiepVu.Dashboard
                         color = "#014D65",
                         dataPoints = new ChartDataPoint[]
                         {
-                            new ChartDataPoint { y = 3, label = "Sweden" },
-                            new ChartDataPoint { y = 7, label = "Taiwan" },
-                            new ChartDataPoint { y = 5, label = "Russia" },
-                            new ChartDataPoint { y = 9, label = "Spain" },
-                            new ChartDataPoint { y = 7, label = "Brazil" },
-                            new ChartDataPoint { y = 7, label = "India" },
-                            new ChartDataPoint { y = 9, label = "Italy" },
-                            new ChartDataPoint { y = 8, label = "Australia" },
-                            new ChartDataPoint { y = 11, label = "Canada" },
-                            new ChartDataPoint { y = 15, label = "South Korea" },
                             new ChartDataPoint { y = 12, label = "Netherlands" },
                             new ChartDataPoint { y = 15, label = "Switzerland" },
                             new ChartDataPoint { y = 25, label = "Britain" },
@@ -123,11 +121,12 @@ namespace LogOne.NghiepVu.Dashboard
 
         private void SaleAnalysis()
         {
-            var id = nameof(SaleAnalysis);
-            Html.Instance.GridCell(6).Div.Id(id).End.End.Render();
+            string id = nameof(SaleAnalysis);
+            Html.Instance.GridCell(4).Panel().Div.Id(id).EndOf(".cell");
 
             var chart = new Chart(id, new ChartOptions
             {
+                height = 300,
                 animationEnabled = true,
                 theme = "light2",
                 title = new ChartTitleOptions { text = "Sales Analysis - June 2019" },
@@ -135,7 +134,7 @@ namespace LogOne.NghiepVu.Dashboard
                 {
                     new ChartDataSeriesOptions {
                         type = "funnel",
-                        indexLabelPlacement = "insider",
+                        indexLabelPlacement = "inside",
                         indexLabelFontColor = "white",
                         toolTipContent =  "<b>{label}</b>: {y} <b>({percentage}%)</b>",
                         indexLabel = "{label} ({percentage}%)",
@@ -157,8 +156,8 @@ namespace LogOne.NghiepVu.Dashboard
         private void CalculatePercentage(Chart chart)
         {
             var dataPoint = chart.options.data[0].dataPoints;
-            var total = dataPoint[0].y;
-            for (var i = 0; i < dataPoint.Length; i++)
+            double? total = dataPoint[0].y;
+            for (int i = 0; i < dataPoint.Length; i++)
             {
                 if (i == 0)
                 {
@@ -169,6 +168,208 @@ namespace LogOne.NghiepVu.Dashboard
                     chart.options.data[0].dataPoints[i]["percentage"] = dataPoint[i].y / total * 100;
                 }
             }
+        }
+
+        private void SaleLeaderBoard()
+        {
+            string id = nameof(SaleLeaderBoard);
+            Html.Instance.GridCell(4).Panel().Div.Id(id).EndOf(".grid");
+
+            var chart = new Chart(id, new ChartOptions
+            {
+                height = 300,
+                animationEnabled = true,
+                theme = "light2",
+                title = new ChartTitleOptions { text = "Sale leaderboard" },
+                axisX = new ChartAxisXOptions { interval = 1 },
+                axisY = new ChartAxisYOptions
+                {
+                    title = "Sale value (USD)"
+                },
+                data = new ChartDataSeriesOptions[]
+                {
+                    new ChartDataSeriesOptions {
+                        type = "bar",
+                        toolTipContent = "<img src=\"https://canvasjs.com/wp-content/uploads/images/gallery/javascript-column-bar-charts/\"{url}\"\" style=\"width:40px; height:20px;\"> <b>{label}</b><br>Budget: ${y}bn<br>{gdp}% of GDP",
+                        indexLabelFontColor = "white",
+                        dataPoints = new ChartDataPoint [] {
+                            new SaleLeaderBoardDataPoint { label= "South Korea", y= 36.8, gdp= 2.7, url= "skorea.png" },
+                            new SaleLeaderBoardDataPoint { label= "Germany", y= 41.1, gdp= 1.2, url= "germany.png" },
+                            new SaleLeaderBoardDataPoint { label= "Japan", y= 46.1, gdp= 1.0, url= "japan.png" },
+                            new SaleLeaderBoardDataPoint { label= "United Kingdom", y= 48.3, gdp= 1.9, url= "uk.png" },
+                            new SaleLeaderBoardDataPoint { label= "India", y= 55.9, gdp= 2.5, url= "india.png" },
+                            new SaleLeaderBoardDataPoint { label= "Russia", y= 69.2, gdp= 5.3, url= "russia.png" },
+                            new SaleLeaderBoardDataPoint { label= "China", y= 215.7, gdp= 1.9, url= "china.png" },
+                            new SaleLeaderBoardDataPoint { label= "United States", y= 611.2, gdp= 3.3, url= "us.png" }
+                        }
+                    }
+                }
+            });
+            chart.render();
+        }
+
+        private void MarketShare()
+        {
+            string id = nameof(MarketShare);
+            Html.Instance.Grid().GridRow().GridCell(4).Panel().Div.Id(id).EndOf(".cell");
+
+            var chart = new Chart(id, new ChartOptions
+            {
+                height = 300,
+                animationEnabled = true,
+                exportEnabled = true,
+                theme = "light2",
+                title = new ChartTitleOptions { text = "Desktop Browser Market Share in 2016" },
+                data = new ChartDataSeriesOptions[]
+                {
+                    new ChartDataSeriesOptions {
+                        type = "pie",
+                        startAngle = 25,
+                        toolTipContent = "<b>{label}</b>: {y}%",
+                        showInLegend= true,
+                        legendText= "{label}",
+                        indexLabelFontSize= 16,
+                        indexLabel= "{label} - {y}%",
+                        dataPoints = new ChartDataPoint [] {
+                            new ChartDataPoint { y= 51.08, label= "Chrome" },
+                            new ChartDataPoint { y= 27.34, label= "Internet Explorer" },
+                            new ChartDataPoint { y= 10.62, label= "Firefox" },
+                            new ChartDataPoint { y= 5.02, label= "Microsoft Edge" },
+                            new ChartDataPoint { y= 4.07, label= "Safari" },
+                            new ChartDataPoint { y= 1.22, label= "Opera" },
+                            new ChartDataPoint { y= 0.44, label= "Others" }
+                        }
+                    }
+                }
+            });
+            chart.render();
+        }
+
+        private void MonthlySaleData()
+        {
+            string id = nameof(MonthlySaleData);
+            Html.Instance.GridCell(4).Panel().Div.Id(id).EndOf(".cell");
+
+            var chart = new Chart(id, new ChartOptions
+            {
+                height = 300,
+                animationEnabled = true,
+                exportEnabled = true,
+                theme = "light2",
+                title = new ChartTitleOptions { text = "Monthly Sales Data" },
+                axisX = new ChartAxisXOptions { valueFormatString = "MMM" },
+                axisY = new ChartAxisYOptions { prefix = "$" },
+                toolTip = new ChartToolTipOptions { shared = true },
+                legend = new ChartLegendOptions { cursor = "pointer" },
+                data = new ChartDataSeriesOptions[]
+                {
+                    new ChartDataSeriesOptions {
+                        type = "column",
+                        name = "Actual Sales",
+                        showInLegend= true,
+                        xValueFormatString= "MMMM YYYY",
+                        yValueFormatString= "$#,##0",
+                        dataPoints = new ChartDataPoint [] {
+                            new ChartDataPoint { x= new Date(2016, 0).GetTime(), y= 20000 },
+                            new ChartDataPoint { x= new Date(2016, 1).GetTime(), y= 30000 },
+                            new ChartDataPoint { x= new Date(2016, 2).GetTime(), y= 25000 },
+                            new ChartDataPoint { x= new Date(2016, 3).GetTime(), y= 70000, indexLabel= "High Renewals" },
+                            new ChartDataPoint { x= new Date(2016, 4).GetTime(), y= 50000 },
+                            new ChartDataPoint { x= new Date(2016, 5).GetTime(), y= 35000 },
+                            new ChartDataPoint { x= new Date(2016, 6).GetTime(), y= 30000 },
+                            new ChartDataPoint { x= new Date(2016, 7).GetTime(), y= 43000 },
+                            new ChartDataPoint { x= new Date(2016, 8).GetTime(), y= 35000 },
+                            new ChartDataPoint { x= new Date(2016, 9).GetTime(), y=  30000},
+                            new ChartDataPoint { x= new Date(2016, 10).GetTime(), y= 40000 },
+                            new ChartDataPoint { x= new Date(2016, 11).GetTime(), y= 50000 }
+                        }
+                    },
+                    new ChartDataSeriesOptions
+                    {
+                        type= "line",
+                        name= "Expected Sales",
+                        showInLegend= true,
+                        yValueFormatString= "$#,##0",
+                        dataPoints = new ChartDataPoint [] {
+                            new ChartDataPoint { x= new Date(2016, 0).GetTime(), y= 40000 },
+			                new ChartDataPoint { x= new Date(2016, 1).GetTime(), y= 42000 },
+			                new ChartDataPoint { x= new Date(2016, 2).GetTime(), y= 45000 },
+			                new ChartDataPoint { x= new Date(2016, 3).GetTime(), y= 45000 },
+			                new ChartDataPoint { x= new Date(2016, 4).GetTime(), y= 47000 },
+			                new ChartDataPoint { x= new Date(2016, 5).GetTime(), y= 43000 },
+			                new ChartDataPoint { x= new Date(2016, 6).GetTime(), y= 42000 },
+			                new ChartDataPoint { x= new Date(2016, 7).GetTime(), y= 43000 },
+			                new ChartDataPoint { x= new Date(2016, 8).GetTime(), y= 41000 },
+			                new ChartDataPoint { x= new Date(2016, 9).GetTime(), y= 45000 },
+			                new ChartDataPoint { x= new Date(2016, 10).GetTime(), y= 42000 },
+                            new ChartDataPoint { x= new Date(2016, 11).GetTime(), y= 50000 }
+                        }
+                    },
+                    new ChartDataSeriesOptions
+                    {
+                        type= "area",
+		                name= "Profit",
+		                markerBorderColor= "white",
+		                markerBorderThickness= 2,
+		                showInLegend= true,
+		                yValueFormatString= "$#,##0",
+                        dataPoints = new ChartDataPoint [] {
+                            new ChartDataPoint { x= new Date(2016, 0).GetTime(), y= 5000 },
+			                new ChartDataPoint { x= new Date(2016, 1).GetTime(), y= 7000 },
+			                new ChartDataPoint { x= new Date(2016, 2).GetTime(), y= 6000},
+			                new ChartDataPoint { x= new Date(2016, 3).GetTime(), y= 30000 },
+			                new ChartDataPoint { x= new Date(2016, 4).GetTime(), y= 20000 },
+			                new ChartDataPoint { x= new Date(2016, 5).GetTime(), y= 15000 },
+			                new ChartDataPoint { x= new Date(2016, 6).GetTime(), y= 13000 },
+			                new ChartDataPoint { x= new Date(2016, 7).GetTime(), y= 20000 },
+			                new ChartDataPoint { x= new Date(2016, 8).GetTime(), y= 15000 },
+			                new ChartDataPoint { x= new Date(2016, 9).GetTime(), y=  10000},
+			                new ChartDataPoint { x= new Date(2016, 10).GetTime(), y= 19000 },
+                            new ChartDataPoint { x= new Date(2016, 11).GetTime(), y= 22000 }
+                        }
+                    }
+                }
+            });
+            chart.render();
+        }
+
+        private void MonthlyExpense()
+        {
+            string id = nameof(MonthlyExpense);
+            Html.Instance.GridCell(4).Panel().Div.Id(id).EndOf(".cell");
+
+            var chart = new Chart(id, new ChartOptions
+            {
+                height = 300,
+                animationEnabled = true,
+                exportEnabled = true,
+                theme = "light2",
+                title = new ChartTitleOptions { text = "Monthly Expense" },
+                axisX = new ChartAxisXOptions { valueFormatString = "MMM" },
+                axisY = new ChartAxisYOptions { prefix = "$" },
+                toolTip = new ChartToolTipOptions { shared = true },
+                legend = new ChartLegendOptions { cursor = "pointer" },
+                data = new ChartDataSeriesOptions[]
+                {
+                    new ChartDataSeriesOptions {
+                        type= "doughnut",
+		                innerRadius= 90,
+		                showInLegend= true,
+		                toolTipContent= "<b>{name}</b>= ${y} (#percent%)",
+		                indexLabel= "{name} - #percent%",
+                        dataPoints = new ChartDataPoint [] {
+                            new ChartDataPoint { y= 450, name= "Food" },
+			                new ChartDataPoint { y= 120, name= "Insurance" },
+			                new ChartDataPoint { y= 300, name= "Travelling" },
+			                new ChartDataPoint { y= 800, name= "Housing" },
+			                new ChartDataPoint { y= 150, name= "Education" },
+			                new ChartDataPoint { y= 150, name= "Shopping"},
+                            new ChartDataPoint { y= 250, name= "Others" }
+                        }
+                    }
+                }
+            });
+            chart.render();
         }
     }
 }
