3a5f64ade8f8c22e08b4eee2599fc5ff05aa304c
NhanNguyen <nhan.htmljs@gmail.com>
2019-11-13 15:11:20 +0700

    Commit to pop stash

---
+++ TMS.API/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using System;
+using TMS.API.Models;
+
+namespace TMS.API.Extensions
+{
+    public static class ElasticsearchExtensions
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0067:Dispose objects before losing scope", Justification = "<Pending>")]
+        public static void AddElasticsearch(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            var url = configuration["elasticsearch:url"];
+            var defaultIndex = configuration["elasticsearch:index"];
+
+            var settings = new ConnectionSettings(new Uri(url))
+                .DefaultIndex(defaultIndex);
+            var client = new ElasticClient(settings);
+            services.AddSingleton<IElasticClient>(client);
+        }
+    }
+}
