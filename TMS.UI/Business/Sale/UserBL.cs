f013aa558c8bb17c02816ef5a3188aa255651ee2
HuuLinh99 <hungbsse130615@fpt.edu.vn>
2020-01-07 11:06:11 +0700

    Linh commit

---
+++ TMS.UI/Business/Sale/UserBL.cs
@@ -0,0 +1,40 @@
+﻿using Common.Clients;
+using Common.Extensions;
+using Common.ViewModels;
+using Components;
+using Components.Forms;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS.API.Models;
+
+namespace TMS.UI.Business.Sale
+{
+    public class UserBL : TabEditor<User>
+    {
+        public UserBL()
+        {
+            Name = "User List";
+            Title = Name;
+        }
+        public void EditUser(User User)
+        {
+            InitUserForm(User);
+        }
+        public void CreateUser(User User)
+        {
+            InitUserForm(User);
+        }
+
+        private void InitUserForm(User user)
+        {
+            var userForm = new PopupEditor<User>
+            {
+                Entity = user,
+                Name = "User Detail",
+                Title = "User Detail"
+            };
+            AddChild(userForm);
+        }
+    }
+}
