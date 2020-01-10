0587037d2d92d334b54b02ce91478805a22d13fa
nttindev <1660618@gmail.com>
2020-01-10 15:33:17 +0700

    add masterdata manage

---
+++ TMS.UI/Business/UnitofMeasure/MasterDataBL.cs
@@ -0,0 +1,22 @@
+﻿using Common.Clients;
+using Common.Extensions;
+using Common.ViewModels;
+using Components;
+using Components.Forms;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS.API.Models;
+
+namespace TMS.UI.Business.UnitofMeaure
+{
+    public class MasterDataBL : TabEditor<UoM>
+    {
+        public MasterDataBL()
+        {
+            Name = "Master Data";
+            Title = Name;
+        }
+
+    }
+}
