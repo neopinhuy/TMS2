ab0ae6f7c1bd5dc7111ab8414611885c790ff109
nttindev <1660618@gmail.com>
2020-01-14 16:28:23 +0700

    add main.js

---
+++ TMS.API/wwwRoot/js/Main.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+	var count = 0;
+	$('button#sidebar-toggle-2').click();
+	$('button#sidebar-toggle-2').click(function (e) {
+		if (count % 2 == 0) {
+			$('.fix-sidebar').width('100%');
+			count = 1;
+		}
+		else {
+			$('.fix-sidebar').width('calc(100% - 290px)');
+			count = 2;
+		}
+	});
+
+})
