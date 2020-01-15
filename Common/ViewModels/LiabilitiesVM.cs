ab955ab251964779c41d21cd7ef471708f033dae
nguyentrantindev@gmail.com <nguyentrantindev@gmail.com>
2020-01-15 17:19:34 +0700

    add liabilities girdview

---
+++ Common/ViewModels/LiabilitiesVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using TMS.API.Models;
+
+namespace Common.ViewModels
+{
+    public class LiabilitiesVM
+    {
+        public int? AccountTypeId { get; set; }
+        public int? TargetTypeId { get; set; }
+        public int? TargetId { get; set; }
+        public DateTime FromDate { get; set; } = DateTime.Now;
+        public DateTime ToDate { get; set; } = DateTime.Now;
+    }
+}
--- TMS.UI/Business/Accounting/LiabilitiesBL.cs
+++ TMS.UI/Business/Accounting/LiabilitiesBL.cs
@@ -12,38 +12,15 @@ namespace TMS.UI.Business.Accounting
 {
     public class LiabilitiesBL : TabEditor<LiabilitiesWarning>
     {
-        private readonly LedgerVM _vm;
-        private readonly string _editorName = $"Edit{typeof(Ledger).Name}";
         public LiabilitiesBL()
         {
             Name = "Liabilities List";
             Title = "Liabilities";
-            //Entity = _vm = new LedgerVM
-            //{
-            //    FromDate = DateTime.Now.AddDays(-DateTime.Now.Day + 1),
-            //};
         }
-        public override void Create()
+        public void Search()
         {
-            var popup = new PopupEditor<Ledger>()
-            {
-                Name = _editorName,
-                Title = "New ledger",
-                Entity = new Ledger() { ExchangeRate = 1 }
-            };
-            AddChild(popup);
-        }
-
-        public void EditLedger(Ledger entity)
-        {
-            var popup = new PopupEditor<Ledger>()
-            {
-                Name = _editorName,
-                Title = "Edit ledger",
-                Entity = entity,
-            };
-            AddChild(popup);
-            popup.Disabled = true;
+            var grid = FindComponent<GridView>().FirstOrDefault();
+            grid?.ReloadData();
         }
     }
 }
