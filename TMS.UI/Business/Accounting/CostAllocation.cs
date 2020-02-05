f78a9fb92a12e4d872502597c70a203427710197
Huy Vo <neo.pinhuy00@gmail.com>
2020-02-05 18:35:28 +0700

    Add cost allocation

---
+++ TMS.UI/Business/Accounting/CostAllocation.cs
@@ -0,0 +1,14 @@
+﻿using Components.Forms;
+using TMS.API.Models;
+
+namespace TMS.UI.Business.Accounting
+{
+    public class CostAllocationBL : TabEditor<Order>
+    {
+        public CostAllocationBL()
+        {
+            Name = "Cost Allocation";
+            Title = "Cost Allocation";
+        }
+    }
+}
