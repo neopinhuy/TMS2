188b5c2f97e16c62ccc4fe8154102b5078725f75
Huy Vo <neo.pinhuy00@gmail.com>
2020-02-06 17:44:46 +0700

    Policy GridView

---
+++ TMS.UI/Business/Sale/PolicyManagementBL.cs
@@ -0,0 +1,13 @@
+﻿using Components.Forms;
+using TMS.API.Models;
+namespace TMS.UI.Business.Sale
+{
+    public class PolicyManagementBL : TabEditor<Policy>
+    {
+        public PolicyManagementBL()
+        {
+            Name = "PolicyManagement List";
+            Title = Name;
+        }
+    }
+}
