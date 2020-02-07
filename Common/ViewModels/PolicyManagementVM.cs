4e9e6141f8c90ea57a57bff1a0fe74fcd6cafdd8
Huy Vo <neo.pinhuy00@gmail.com>
2020-02-07 14:56:00 +0700

    edit Policy Management

---
+++ Common/ViewModels/PolicyManagementVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TMS.API.Models;
+
+namespace Common.ViewModels
+{
+    public class PolicyManagementVM
+    {
+        public List<Policy> ListPolicy { get; set; }
+    }
+}
--- TMS.UI/Business/Sale/PolicyManagementBL.cs
+++ TMS.UI/Business/Sale/PolicyManagementBL.cs
@@ -1,13 +1,34 @@
-﻿using Components.Forms;
+﻿using Common.Clients;
+using Common.Extensions;
+using Common.ViewModels;
+using Components.Forms;
+using System.Threading.Tasks;
 using TMS.API.Models;
 namespace TMS.UI.Business.Sale
 {
-    public class PolicyManagementBL : TabEditor<Policy>
+    public class PolicyManagementBL : TabEditor<PolicyManagementVM>
     {
         public PolicyManagementBL()
         {
-            Name = "PolicyManagement List";
+            Entity = new PolicyManagementVM();
+            Name = "ListPolicy";
             Title = Name;
+        }
+
+        public override async Task<bool> Save(bool defaultMessage = false)
+        {
+            var vm = Entity as PolicyManagementVM;
+            // Save changes to features
+            var ok = await Client<Policy>.Instance.BulkUpdateAsync(vm.ListPolicy);
+            if (ok)
+            {
+                Toast.Success("Save feature succeeded!");
+            }
+            else
+            {
+                Toast.Warning("Save feature failed!");
+            }
+            return true;
         }
     }
 }
