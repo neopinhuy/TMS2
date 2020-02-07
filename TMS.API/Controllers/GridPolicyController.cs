b6379ff58938c78bb9e5fa14cd76eccf2d19d2f2
NhanNguyen <nhan.htmljs@gmail.com>
2020-02-07 09:13:36 +0700

    Fix load grid policy in Component detail config

--- TMS.API/Controllers/GridPolicyController.cs
+++ TMS.API/Controllers/GridPolicyController.cs
@@ -1,14 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Nest;
+using System.Threading.Tasks;
 using TMS.API.Models;
+using System.Linq;
+using Microsoft.AspNet.OData.Query;
 
 namespace TMS.API.Controllers
 {
-    
     public class GridPolicyController : GenericController<GridPolicy>
     {
         public GridPolicyController(TMSContext context, IElasticClient client) : base(context, client)
         {
+        }
+
+        [HttpGet("api/[Controller]/GetByComponentGroupId/{id}")]
+        public async Task<IActionResult> GetGridPolicyByComponentGroup(int id, ODataQueryOptions<GridPolicy> options)
+        {
+            var query =
+                from cg in db.ComponentGroup
+                join com in db.Component on cg.Id equals com.ComponentGroupId
+                join feature in db.Feature on cg.FeatureId equals feature.Id
+                join gridPolicy in db.GridPolicy on feature.Id equals gridPolicy.FeatureId
+                where cg.Id == id && com.ComponentType == "GridView" && gridPolicy.EntityId == com.ReferenceId
+                select gridPolicy;
+            return await ApplyCustomQuery(options, query);
         }
     }
 }
